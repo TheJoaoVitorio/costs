ba74df727148638714921755dd3eef99b1408909
TheJoaoVitorio <joaovtr999x@gmail.com>
2024-09-15 16:27:36 -0300

    Home creation

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,6 +1,19 @@
+import styles from './Home.module.css'
+import Savings from '../../img/savings.svg'
+import LinkButton from '../layout/LinkButton'
+
 function Home(){
     return(
-        <h1>Home</h1>
+        <section className={styles.homeContainer} >
+            <h1>Welcome to <span>Costs</span></h1>
+            <p>Start managing your projects right now!</p>
+            <LinkButton
+                to="/newproject"
+                text="Create Project"
+            />
+            <img src={Savings} ></img>
+        </section>
+
     )
 }
 
