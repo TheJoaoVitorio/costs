ff42aa0c49fe97ea7ef08ea75063bd9e146b701e
TheJoaoVitorio <joaovtr999x@gmail.com>
2024-09-29 02:00:06 -0300

    add lottie

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,20 +1,26 @@
-import styles from './Home.module.css'
-import Savings from '../../img/savings.svg'
-import LinkButton from '../layout/LinkButton'
+import styles from './Home.module.css';
+import React from 'react';
+import Lottie from 'lottie-react'; // Importa o Lottie corretamente
+import LottieHome from '../../lotties/lottiePayments.json';
 
-function Home(){
-    return(
-        <section className={styles.homeContainer} >
+import LinkButton from '../layout/LinkButton';
+
+function Home() {
+    return (
+        <section className={styles.homeContainer}>
             <h1>Welcome to <span>Costs</span></h1>
             <p>Start managing your projects right now!</p>
             <LinkButton
                 to="/newproject"
                 text="Create Project"
             />
-            <img src={Savings} ></img>
+            <Lottie 
+                animationData={LottieHome} // Arquivo JSON da animação
+                loop={true} 
+                style={{ width: 300, height: 300 }} // Estilo da animação
+            />
         </section>
-
-    )
+    );
 }
 
-export default Home
+export default Home;
